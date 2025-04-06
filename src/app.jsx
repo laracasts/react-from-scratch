9024ffc23ea635c3d64a8c5b61476cfb9c7d9e27
Simon Vrachliotis <simon@simonswiss.com>
2025-04-07 08:46:03 +1000

    episode-6

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Hello React and Vite!</h1>
+}
+
+const app = document.getElementById('app')
+const root = ReactDOM.createRoot(app)
+
+root.render(<App />)
