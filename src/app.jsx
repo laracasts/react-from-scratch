2328e09a7b7f2c7dd96a57a9b0fbf04995652bd7
Simon Vrachliotis <simon@simonswiss.com>
2025-04-07 09:49:59 +1000

    episode-8

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import './app.css'
+const tech = "React, Tailwind and Vite";
 
-function App() {
-  return <h1 className="font-mono text-4xl">Hello React and Vite!</h1>
+export function App() {
+  return <h1 className="font-mono text-4xl">Hello {tech}!</h1>;
 }
-
-const app = document.getElementById('app')
-const root = ReactDOM.createRoot(app)
-
-root.render(<App />)
